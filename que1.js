c8b3199dfbd78e66a7e98598d458ec3da9b82479
Om Prakash Kumawat <mrop18@gmail.com>
2026-01-03 21:42:19 +0530

    added que1 call stack

---
+++ que1.js
@@ -0,0 +1,3 @@
+// What is call stack? Use one real life analogy. 
+
+// ANS : Call stack is structure that tracks functions execution. Its functionality can be understand by stacking CD drives in holder where the last comes in will be first to go out. 
